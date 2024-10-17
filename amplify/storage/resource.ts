820f37d657d685aa89c07e5f5af77b31977b11a1
DonteDoesAI <dontavious.jennings@neuralsolutions.ai>
2024-10-17 18:52:48 -0400

    test

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,7 @@ export const resumesBucket = defineStorage({
     name: 'resumesBucket',
     isDefault: true, // identify your default storage bucket (required)
     access: (allow) => ({
-      '*/*': [
+      'resumes/*': [
         allow.authenticated.to(['read', 'write', 'delete'])
       ]
     })
@@ -13,7 +13,7 @@ export const resumesBucket = defineStorage({
   export const profilesBucket = defineStorage({
     name: 'profilesBucket',
     access: (allow) => ({
-      '*/*': [
+      'profiles/*': [
         allow.authenticated.to(['read', 'write', 'delete'])
       ]
     })
@@ -22,7 +22,7 @@ export const resumesBucket = defineStorage({
   export const jobReqsBucket = defineStorage({
     name: 'jobReqsBucket',
     access: (allow) => ({
-      '*/*': [
+      'jobReqs/*': [
         allow.authenticated.to(['read', 'write', 'delete'])
       ]
     })
